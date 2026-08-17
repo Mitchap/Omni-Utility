7be9c5a0c1fdf5bbb82dbddd3c7060bce5b9639e
mitchap <mitchbarcenilla4@gmail.com>
2026-08-17 23:05:43 +0800

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace omni_multitool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
