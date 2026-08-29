257ee6d88f74a34c5430c8788b4373c34f9f50be
mitchap <mitchbarcenilla4@gmail.com>
2026-08-29 21:05:04 +0800

    Added water reminder notif sound and test button on tool settings

--- Features/WaterReminder/WaterReminderPage.xaml.cs
+++ Features/WaterReminder/WaterReminderPage.xaml.cs
@@ -1,26 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Runtime.Versioning;
 
 namespace omni_multitool.Features.WaterReminder
 {
-    /// <summary>
-    /// Interaction logic for WaterReminder.xaml
-    /// </summary>
+    [SupportedOSPlatform("windows")]
     public partial class WaterReminderPage : Page
     {
+        private readonly MediaPlayer _reminderSound;
+
         public WaterReminderPage()
         {
             InitializeComponent();
+
+            string soundPath = System.IO.Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Assets",
+                "Audio",
+                "mixkit-uplifting-flute.wav");
+
+            _reminderSound = new MediaPlayer
+            {
+                Volume = 0.35
+            };
+
+            _reminderSound.Open(new Uri(soundPath));
+        }
+
+        private void BtnPlaySound_Click(object sender, RoutedEventArgs e)
+        {
+            _reminderSound.Position = TimeSpan.Zero;
+            _reminderSound.Play();
         }
     }
 }
